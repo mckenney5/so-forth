70fb7b9f730d4695f504694255f7325f1f6b4af3
mckenney5 <mckenneyadam@gmail.com>
2021-06-15 00:17:04 -0400

    Made ltemp (registers) an array, shortened 2swap code

--- src/words.c
+++ src/words.c
@@ -13,10 +13,8 @@ int check_words(char *word){
  * returns 1 if a command was found, returns 0 if it wasnt
  * add new words above and here, dont forget to add them to the header
  */
-	long ltemp = 0; //TODO make this an array
-	long ltemp2 = 0;
-	long ltemp3 = 0;
-	long ltemp4 = 0;
+	long ltemp[5] = {0};
+ 
 	char* stemp = 0;
 	size_t i = 0;
 	
@@ -26,30 +24,30 @@ int check_words(char *word){
 	else if(!strcmp("2dup", word)){ push(read_stack(1)); push(read_stack(1));}
 	else if(!strcmp("drop", word)) pop(0);
 	else if(!strcmp("2drop", word)){ pop(0); pop(0);}
-	else if(!strcmp("spaces", word)){ ltemp = pop(0); if(!get_error()) for(i=0; (long)i < ltemp; i++) putchar(' ');}
+	else if(!strcmp("spaces", word)){ ltemp[0] = pop(0); if(!get_error()) for(i=0; (long)i < ltemp[0]; i++) putchar(' ');}
 	else if(!strcmp("emit", word)) putchar((char)pop(0));
 	else if(!strcmp("cr", word)) putchar('\n');
 	else if(!strcmp("space", word)) putchar(' ');
 	else if(!strcmp("mod", word)) handle_operator('%');
-	else if(!strcmp("type", word)){ ltemp = pop(0); stemp = (char*)pop(0); for(i=0; (long)i < ltemp; i++)putchar(stemp[i]); pop_string(stemp);} //TODO check for segfaults?
-	else if(!strcmp(".", word)){ ltemp = pop(0); if(!get_error()) printf("%ld", ltemp); fflush(stdout);}
+	else if(!strcmp("type", word)){ ltemp[0] = pop(0); stemp = (char*)pop(0); for(i=0; (long)i < ltemp[0]; i++)putchar(stemp[i]); pop_string(stemp);} //TODO check for segfaults?
+	else if(!strcmp(".", word)){ ltemp[0] = pop(0); if(!get_error()) printf("%ld", ltemp[0]); fflush(stdout);}
 	else if(!strcmp(".s", word)) show_stack();
 	else if(!strcmp("*/", word)) push((read_stack(2) * read_stack(1) / pop(0)));
 	else if(!strcmp("page", word)) printf("[H[2J[3J"); //TODO not portable (from bash: clear > out.txt)
 	else if(!strcmp("true", word)) push(_TRUE);
 	else if(!strcmp("false", word)) push(_FALSE);
 	else if(!strcmp("invert", word)) push(~ pop(0));
-	else if(!strcmp("<>", word)){ ltemp = pop(0); if((pop(0) ^ ltemp) != 0) push(_TRUE); else push(_FALSE);} //XOR's to check bit-for-bit not equal to
+	else if(!strcmp("<>", word)){ ltemp[0] = pop(0); if((pop(0) ^ ltemp[0]) != 0) push(_TRUE); else push(_FALSE);} //XOR's to check bit-for-bit not equal to
 	else if(!strcmp("0<", word)) if(pop(0) < 0) push(_TRUE); else push(_FALSE);
 	else if(!strcmp("0=", word)) if(pop(0) == 0) push(_TRUE); else push(_FALSE);
 	else if(!strcmp("1+", word)) push(pop(0) +1);
 	else if(!strcmp("1-", word)) push(pop(0) -1);
 	else if(!strcmp("abs", word)){ if(read_stack(0) < 0) push(pop(0) * -1);}
-	else if(!strcmp("xor", word)){ ltemp = pop(0); push(ltemp ^ pop(0)); }
-	else if(!strcmp("or", word)){ ltemp = pop(0); push(ltemp | pop(0)); }
-	else if(!strcmp("and", word)){ ltemp = pop(0); push(ltemp & pop(0)); }
-	else if(!strcmp("swap", word)){ ltemp = pop(0); ltemp2 = pop(0); push(ltemp); push(ltemp2); }
-	else if(!strcmp("2swap", word)){ ltemp = pop(0); ltemp2 = pop(0); ltemp3 = pop(0); ltemp4 = pop(0); push(ltemp2); push(ltemp); push(ltemp4); push(ltemp3); }
+	else if(!strcmp("xor", word)){ ltemp[0] = pop(0); push(ltemp[0] ^ pop(0)); }
+	else if(!strcmp("or", word)){ ltemp[0] = pop(0); push(ltemp[0] | pop(0)); }
+	else if(!strcmp("and", word)){ ltemp[0] = pop(0); push(ltemp[0] & pop(0)); }
+	else if(!strcmp("swap", word)){ ltemp[0] = pop(0); ltemp[2] = pop(0); push(ltemp[0]); push(ltemp[2]); }
+	else if(!strcmp("2swap", word)){ for(i = 0; i < 4; i++) ltemp[i] = pop(0); push(ltemp[1]); push(ltemp[0]); push(ltemp[3]); push(ltemp[2]); }
 
 	// -- Non Standard --
 	else if(!strcmp("system", word)){ pop(0); system((char*)pop(0));} //NON-STANDARD
